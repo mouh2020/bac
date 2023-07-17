9c6eeb64c1fc15ad236db0cacd6e0b6575404406
mouh2020 <badimohammed2019@gmail.com>
2023-07-17 21:20:03 +0100

    usage example

---
+++ example.py
@@ -0,0 +1,16 @@
+from bac.bac import Bac
+
+student = Bac(matricule="12345678",
+              code="123")
+
+## Return student informations
+result = student.condidate_result_json()
+print(result['الاسم'])
+print(result['اللقب'])
+print(result['المعدل'])
+print(result['الملاحظة'])
+print(result['الشعبة'])
+print(result['رقم التسجيل'])
+## Return if the student succeed or not (True if succed)
+succeed = student.check_condidate_success()
+## Return if the student succeed or not (True if succed)
